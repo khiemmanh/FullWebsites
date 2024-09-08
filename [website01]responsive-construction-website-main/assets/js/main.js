d691010a5efacbf6c7b156d87401a31d37f9c621
khiem.manh.298 <khiem.manh.298@gmail.com>
2024-09-08 23:12:51 +0700

    [website01] toggle menu for nav mobile devices

--- [website01]responsive-construction-website-main/assets/js/main.js
+++ [website01]responsive-construction-website-main/assets/js/main.js
@@ -1,5 +1,23 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav_toggle'),
+        navClose = document.getElementById('nav-close');
 
+// show menu
+if(navToggle){
+    console.log(navClose);
+    
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// hidden menu
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
